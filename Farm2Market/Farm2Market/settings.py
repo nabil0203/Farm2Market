ec411636a0fd20af0b8a185e281d8c6e173afd9a
Nabil Ahmed <nabilahmed0203@gmail.com>
2026-03-04 10:59:27 +0600

    Added templates

--- Farm2Market/Farm2Market/settings.py
+++ Farm2Market/Farm2Market/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Farm2Market.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
